6ed3bf2e3305f12abe4f59ee7476de5cf6d7887f
marat1000 <marat76593@gmail.com>
2022-05-02 19:48:57 +0300

    refactor: minor fixes

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -24,6 +24,12 @@ class VigenereCipheringMachine {
     this.type = type;
     this.CODE_FL = 65;
     this.CODE_LL = 90;
+    this.ALF = ``;
+    let i = this.CODE_FL;
+    while (i !== this.CODE_LL + 1) {
+      this.ALF = this.ALF + String.fromCharCode(i);
+      i++;
+    }
   }
 
   encrypt(message, key) {
@@ -35,8 +41,8 @@ class VigenereCipheringMachine {
     let messageToUp = message.toUpperCase();
     let keyToUp = key.toUpperCase();
     for (let letter of messageToUp) {
-      if (letter.charCodeAt(0) >= this.CODE_FL && letter.charCodeAt(0) <= this.CODE_LL) {
-        res += String.fromCharCode((letter.charCodeAt(0) + keyToUp.charCodeAt(count % keyToUp.length)) % (this.CODE_LL - this.CODE_FL + 1) + this.CODE_FL);
+      if (this.ALF.indexOf(letter) > -1) {
+        res += this.ALF[(letter.charCodeAt(0) + keyToUp.charCodeAt(count % keyToUp.length)) % this.ALF.length];
         count++;
       } else {
         res += letter;
@@ -57,8 +63,8 @@ class VigenereCipheringMachine {
     let messageToUp = message.toUpperCase();
     let keyToUp = key.toUpperCase();
     for (let letter of messageToUp) {
-      if (letter.charCodeAt(0) >= this.CODE_FL && letter.charCodeAt(0) <= this.CODE_LL) {
-        res += String.fromCharCode( (letter.charCodeAt(0) + (this.CODE_LL - this.CODE_FL + 1) - keyToUp.charCodeAt(count % keyToUp.length)) % (this.CODE_LL - this.CODE_FL + 1) + this.CODE_FL);
+      if (this.ALF.indexOf(letter) > -1) {
+        res += this.ALF[(letter.charCodeAt(0) + this.ALF.length - keyToUp.charCodeAt(count % keyToUp.length)) % this.ALF.length];
         count++;
       }
       else {
@@ -70,6 +76,50 @@ class VigenereCipheringMachine {
     }
     return res;
   }
+  // encrypt(message, key) {
+  //   if (typeof message !== `string` || typeof key !== `string`) {
+  //     throw new Error(`Incorrect arguments!`)
+  //   }
+  //   let res = ``;
+  //   let count = 0;
+  //   let messageToUp = message.toUpperCase();
+  //   let keyToUp = key.toUpperCase();
+  //   for (let letter of messageToUp) {
+  //     if (letter.charCodeAt(0) >= this.CODE_FL && letter.charCodeAt(0) <= this.CODE_LL) {
+  //       res += String.fromCharCode((letter.charCodeAt(0) + keyToUp.charCodeAt(count % keyToUp.length)) % (this.CODE_LL - this.CODE_FL + 1) + this.CODE_FL);
+  //       count++;
+  //     } else {
+  //       res += letter;
+  //     }
+  //   }
+  //   if (this.type === false) {
+  //     res = res.split(``).reverse().join(``);
+  //   }
+  //   return res;
+  // }
+  //
+  // decrypt(message, key) {
+  //   if (typeof message !== `string` || typeof key !== `string`) {
+  //     throw new Error(`Incorrect arguments!`)
+  //   }
+  //   let res = ``;
+  //   let count = 0;
+  //   let messageToUp = message.toUpperCase();
+  //   let keyToUp = key.toUpperCase();
+  //   for (let letter of messageToUp) {
+  //     if (letter.charCodeAt(0) >= this.CODE_FL && letter.charCodeAt(0) <= this.CODE_LL) {
+  //       res += String.fromCharCode( (letter.charCodeAt(0) + (this.CODE_LL - this.CODE_FL + 1) - keyToUp.charCodeAt(count % keyToUp.length)) % (this.CODE_LL - this.CODE_FL + 1) + this.CODE_FL);
+  //       count++;
+  //     }
+  //     else {
+  //       res += letter;
+  //     }
+  //   }
+  //   if (this.type === false) {
+  //     res = res.split(``).reverse().join(``);
+  //   }
+  //   return res;
+  // }
 }
 
 module.exports = {
